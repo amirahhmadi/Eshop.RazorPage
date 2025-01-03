201a3f15b7e6829f8d666bc0454cb05d45be3d2a
amirahmadi <amirrezaahmadi869@gmail.com>
2025-01-03 13:09:02 +0330

    Add project files.

---
+++ Eshop.RazorPage/Models/Banners/EditBannerCommand.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Eshop.RazorPage.Models.Banners;
+
+public class EditBannerCommand
+{
+    public long Id { get; set; }
+    [Display(Name = "لینک")]
+    [Required(ErrorMessage = "{0} را وارد کنید")]
+    [DataType(DataType.Url)]
+    public string Link { get; set; }
+    public BannerPosition Position { get; set; }
+
+    [Display(Name = "عکس")]
+    public IFormFile? ImageFile { get; set; }
+}
